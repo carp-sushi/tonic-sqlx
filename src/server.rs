babebb63e1b9ae73b6174482a2920e5d2c0e0ffd
carp-sushi <105142707+carp-sushi@users.noreply.github.com>
2025-10-01 18:08:20 -0600

    imports and naming

--- src/server.rs
+++ src/server.rs
@@ -7,16 +7,13 @@ use crate::{
 };
 
 use sqlx::postgres::PgPool;
-use std::{net::SocketAddr, sync::Arc};
+use std::{error::Error, net::SocketAddr, sync::Arc};
 use tonic::{
     codec::CompressionEncoding::Gzip, service::interceptor::InterceptedService, transport::Server,
 };
 
 /// Start the gRPC server.
-pub async fn serve(
-    pool: Arc<PgPool>,
-    grpc_listen_addr: SocketAddr,
-) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn serve(pool: Arc<PgPool>, listen_addr: SocketAddr) -> Result<(), Box<dyn Error>> {
     // Start health check
     let (reporter, health_service) = tonic_health::server::health_reporter();
     tokio::spawn(health_check(reporter, Arc::clone(&pool)));
@@ -36,12 +33,12 @@ pub async fn serve(
     let gsdx_service = InterceptedService::new(gsdx_service_server, RequestInterceptor::new());
 
     // Serve gRPC services
-    log::info!("Server listening on {}", grpc_listen_addr);
+    log::info!("Server listening on {}", listen_addr);
     Server::builder()
         .add_service(health_service)
         .add_service(reflection_service)
         .add_service(gsdx_service)
-        .serve(grpc_listen_addr)
+        .serve(listen_addr)
         .await?;
 
     Ok(())
