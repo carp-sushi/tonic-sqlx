11a47931f227f4a7596bb62d40f0e848afda6570
carp-sushi <105142707+carp-sushi@users.noreply.github.com>
2025-12-01 21:06:22 -0700

    add server struct

--- src/server.rs
+++ src/server.rs
@@ -9,37 +9,52 @@ use crate::{
 use sqlx::postgres::PgPool;
 use std::{error::Error, net::SocketAddr, sync::Arc};
 use tonic::{
-    codec::CompressionEncoding::Gzip, service::interceptor::InterceptedService, transport::Server,
+    codec::CompressionEncoding::Gzip, service::interceptor::InterceptedService,
+    transport::Server as TransportServer,
 };
 
-/// Start the gRPC server.
-pub async fn serve(pool: Arc<PgPool>, listen_addr: SocketAddr) -> Result<(), Box<dyn Error>> {
-    // Start health check
-    let (reporter, health_service) = tonic_health::server::health_reporter();
-    tokio::spawn(health_check(reporter, Arc::clone(&pool)));
+/// GSDX gRPC server.
+pub struct Server {
+    pool: Arc<PgPool>,
+}
 
-    // Set up gRPC reflection
-    let reflection_service = tonic_reflection::server::Builder::configure()
-        .register_encoded_file_descriptor_set(GSDX_V1_FILE_DESCRIPTOR_SET)
-        .build_v1()?;
+impl Server {
+    /// Create a new GSDX gRPC server instance.
+    pub fn new(pool: Arc<PgPool>) -> Self {
+        Self { pool }
+    }
+}
 
-    // Setup the GSDX service with gzip compression.
-    let repo = Repo::new(Arc::clone(&pool));
-    let gsdx_service_server = GsdxServiceServer::new(Service::new(repo))
-        .send_compressed(Gzip)
-        .accept_compressed(Gzip);
+impl Server {
+    /// Start the GSDX gRPC server.
+    pub async fn serve(&self, listen_addr: SocketAddr) -> Result<(), Box<dyn Error>> {
+        // Start health check
+        let (reporter, health_service) = tonic_health::server::health_reporter();
+        tokio::spawn(health_check(reporter, Arc::clone(&self.pool)));
 
-    // Wrap server with request interceptor.
-    let gsdx_service = InterceptedService::new(gsdx_service_server, RequestInterceptor::new());
+        // Set up gRPC reflection
+        let reflection_service = tonic_reflection::server::Builder::configure()
+            .register_encoded_file_descriptor_set(GSDX_V1_FILE_DESCRIPTOR_SET)
+            .build_v1()?;
 
-    // Serve gRPC services
-    tracing::info!("Server listening on {}", listen_addr);
-    Server::builder()
-        .add_service(health_service)
-        .add_service(reflection_service)
-        .add_service(gsdx_service)
-        .serve(listen_addr)
-        .await?;
+        // Setup the GSDX service with gzip compression.
+        let repo = Repo::new(Arc::clone(&self.pool));
+        let gsdx_service_server = GsdxServiceServer::new(Service::new(repo))
+            .send_compressed(Gzip)
+            .accept_compressed(Gzip);
 
-    Ok(())
+        // Wrap server with request interceptor.
+        let gsdx_service = InterceptedService::new(gsdx_service_server, RequestInterceptor::new());
+
+        // Serve gRPC services
+        tracing::info!("Server listening on {}", listen_addr);
+        TransportServer::builder()
+            .add_service(health_service)
+            .add_service(reflection_service)
+            .add_service(gsdx_service)
+            .serve(listen_addr)
+            .await?;
+
+        Ok(())
+    }
 }
